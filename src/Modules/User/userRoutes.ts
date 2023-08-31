0738254f7eebca7561c2c2bc15d187ec3b787bb0
lonewolf3199 <98878158+lonewolf3199@users.noreply.github.com>
2023-08-31 18:21:13 +0530

    changes

---
+++ src/Modules/User/userRoutes.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import authController from "../Authentication/authController";
+import userController from "./userController";
+import authentication from "../../MIddlewares/authentication";
+import userValidation from "../../Validations/userValidation";
+
+const router = express.Router();
+
+router
+.post('/signup', userValidation.registerValidate, authController.register)
+
+router
+.post('/login', userValidation.loginValidate, authController.login)
+
+router
+.get('/', authentication.toAdmin,userController.getAllUser);
+
+router
+.get('/user', userController.getMe);
+
+router
+.get('/:id', authentication.toAdmin, userController.getOneUser)
+
+router
+.patch('/userUpdate', userValidation.updateUser, userController.updateCurrentUser)
+
+router
+.patch('/:id', userValidation.updateUser, userController.updateUser)
+
+router
+.delete('/:id', authentication.toAdmin, userController.deleteUser)
