2a3373435843ed26f05e3af52b86c8efc2a19303
lonewolf3199 <98878158+lonewolf3199@users.noreply.github.com>
2023-09-05 17:05:49 +0530

    product routes and validations

--- src/Modules/Product/productRoute.ts
+++ src/Modules/Product/productRoute.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import productController from "./productController";
 import authentication from "../../Middlewares/authentication";
+import productValidation from "../../Validations/productValidation";
 
 const router = express.Router();
+
+
 
 export default router
--- src/Validations/productValidation.ts
+++ src/Validations/productValidation.ts
@@ -10,6 +10,50 @@ const productCreateValidation = (req: Request, res: Response, next: NextFunction
         name: joi.string().required(),
         price: joi.number().required(),
         category: joi.string().required(),
-        
-    })
+        description: joi.string().default( '' ),
+        stock: joi.number().default(0),
+        status: joi.string().valid('active', 'inactive').default('active')
+    });
+    const { error, value} = productSchema.validate(req.body);
+    if(error){
+        return validationErrorResponse(res, error.details[0].message)
+    };
+    res.locals.product = value;
+    next()
+};
+
+const updateProduct = (req: Request, res: Response, next: NextFunction) => {
+    const updateSchema = joi.object({
+        name: joi.string(),
+        price: joi.number(),
+        category: joi.string(),
+        description: joi.string(),
+        stock: joi.number(),
+        status: joi.string().valid('active', 'inactive')
+    });
+    const { error, value} = updateSchema.validate(req.body)
+    if(error){
+        return validationErrorResponse(res, error.details[0].message)
+    };
+    res.locals.product = value;
+    next();
+};
+
+const addOnCart = (req: Request, res: Response, next : NextFunction) => {
+    const cartQuantity = joi.object({
+        productId: joi.number().required(),
+        quantity: joi.number().required()
+    });
+    const { error, value } = cartQuantity.validate(req.body)
+    if(error){
+        return validationErrorResponse(res, error.details[0].message)
+    };
+    res.locals.cart = value
+    next();
+};
+
+export default {
+    productCreateValidation,
+    updateProduct,
+    addOnCart
 }
